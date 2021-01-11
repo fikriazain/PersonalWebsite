3509aae2dfcfb45f8342cd9e980c6c4ca24f1fcf
fikriazain <fikri.azain@gmail.com>
2021-01-11 22:49:28 +0700

    MEH

--- MyWebsite/views.py
+++ MyWebsite/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 def index(request):
     context = {
         'Judul':'Selamat Datang'
-
+        'headline':'LOL'
     }
     return render(request, 'index.html', context)
