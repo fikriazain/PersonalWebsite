e397a990bcea153e89bdacdd35dcdc2f505542c4
Fikri Aufaa Zain <fikri.azain@gmail.com>
2021-02-02 20:10:30 +0700

    newpush

--- MyWebsite/urls.py
+++ MyWebsite/urls.py
@@ -1,9 +1,8 @@
 
+from django.urls import path, include
 from django.contrib import admin
-from django.urls import path
-from . import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', include('homepage.urls')),
 ]
